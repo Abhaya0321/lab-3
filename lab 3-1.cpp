7715eba3d052d74df0b354f49d7cda26f45acb14
Abhaya0321 <31515214+Abhaya0321@users.noreply.github.com>
2017-09-06 18:36:13 +0530

    Add files via upload

---
+++ lab 3-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+   {int x; 
+   cout<<"Enter Number:";
+   cin>>x;
+   if(x>0)
+      cout<<"Number is Positive";
+   else if(x<0)
+      cout<<"Number is Negative";
+   else
+   cout<<"zero";
+   }
+}
